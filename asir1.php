610119b92954c3eff86e2cf8308f1386f7c250b8
gargiljavi <gargiljavi@gmail.com>
2019-09-25 09:08:00 +0200

    version php

---
+++ asir1.php
@@ -0,0 +1,11 @@
+<?php
+echo '<p>Hola esta es la activdad de Javier Garcia</p>'; 
+echo "<br>";
+echo "<a href='http://192.168.0.94/asir1_sergio/asir1.php'>Sergio</a>";
+$file = fopen("visitas.txt","a");
+$ip=$_SERVER['REMOTE_ADDR'];
+$dates = date('H:i:s Y-m-d ');
+$datos = "La ip es ".$ip." con fecha ".$dates."\r\n";
+echo fwrite($file,$datos);
+fclose($file);
+?> 
