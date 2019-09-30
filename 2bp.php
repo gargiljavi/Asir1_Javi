4233d17a04c1680beaa7e17ede6308fb42c3d82d
gargiljavi <gargiljavi@gmail.com>
2019-09-30 12:46:47 +0200

    versión 2c.php

---
+++ 2bp.php
@@ -0,0 +1,10 @@
+<?php
+function calcula($a,$b){
+	echo "Suma:".$a."+".$b."=".($a+$b).'</br>';
+	echo "Resta:".$a."-".$b."=".($a-$b).'</br>';
+	echo "Multiplicar:".$a."*".$b."=".($a*$b).'</br>';
+	echo "Division:".$b."/".$a."=".($b/$a).'</br>';
+}
+calcula(18,20);
+calcula(19,21);
+?>
