4ea6731acc0ec8470837ba8484f36ea8e3852a55
gargiljavi <gargiljavi@gmail.com>
2019-11-14 08:24:27 +0100

    version triandulo e final

--- triangulo_e.php
+++ triangulo_e.php
@@ -9,12 +9,12 @@ function longitud ($d,$e,$f) {
 	return $r;
 }
 function Angulo ($a,$b,$c) {
-		$r="Triangulo Acutangulo";
-			if ($a==90 or $b==90 or $c==90)
-				$r="Triangulo Rectangulo";
-			if ($a>90 or $b>90 or $c>90)
-				$r="Triangulo Obtusangulo";
-			return $r;
+	$r="Triangulo Acutangulo";
+	if ($a==90 or $b==90 or $c==90)
+		$r="Triangulo Rectangulo";
+	if ($a>90 or $b>90 or $c>90)
+		$r="Triangulo Obtusangulo";
+	return $r;
 }
 
 function triangulos($l){
@@ -24,25 +24,26 @@ function triangulos($l){
 	
 	else{
 
-	list($a,$b,$c)=$l;
+		list($a,$b,$c)=$l;
 
-	$x=($a**2-$b**2+$c**2)/(2*$c);
-	$h=sqrt($a**2-$x**2);
-	$A=atan($h/($c-$x))*360/(2*pi());
-	$B=atan($h/($c-$x))*360/(2*pi());
-	$C=180-$A-$B;
-
-	
-	$r=[
-		'angulos'=>[$A,$B,$C],
-		'tipo_lados'=>longitud($a,$b,$c),
-		'tipo_angulos'=>Angulo($A,$B,$C),
-	];
+		$x=($a**2-$b**2+$c**2)/(2*$c);
+		$h=sqrt($a**2-$x**2);
+		$A=atan($h/($c-$x))*360/(2*pi());
+		$B=atan($h/($c-$x))*360/(2*pi());
+		$C=180-$A-$B;
 
 	
-	return $r;
+		$r=[
+			'angulos'=>[$A,$B,$C],
+			'tipo_lados'=>longitud($a,$b,$c),
+			'tipo_angulos'=>Angulo($A,$B,$C),
+		];
+
+		
+	
 	}
-}
+	return $r;
+}	
 
 #$r=triangulos([1,1,sqrt(2)]);
 
@@ -55,12 +56,12 @@ function triangulos($l){
 	
 	
 	
-	function prueba($t){
-$r=triangulos($t);
-echo '<pre>';
-print_r($t);
-print_r($r);
-echo '</pre>';
+function prueba($t){
+	$r=triangulos($t);
+	echo '<pre>';
+	print_r($t);
+	print_r($r);
+	echo '</pre>';
 }
 
 echo '<h1>Isósceles-Rectángulo</h1>';prueba([1,1,sqrt(2)]);
